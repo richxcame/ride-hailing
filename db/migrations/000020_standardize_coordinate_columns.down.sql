095b302e347cea515a91c2fe6e0fdfd9b618626a
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2026-02-16 21:00:34 +0500

    feat: remove duplicated migration files

--- db/migrations/000020_standardize_coordinate_columns.down.sql
+++
@@ -1,4 +0,0 @@
--- Rollback: Revert coordinate column names to abbreviated form
-
-ALTER TABLE cancellation_records RENAME COLUMN pickup_latitude TO pickup_lat;
-ALTER TABLE cancellation_records RENAME COLUMN pickup_longitude TO pickup_lng;
